cd1c653c9b8f264ee48b5c43bb8c3b941f844873
Dorian Cantzen <cantzen@googlemail.com>
2019-02-22 18:55:10 +0100

    add Filepathchecker

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.11 AS build
+ADD . /src
+WORKDIR /src
+RUN go get -d -v -t
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o go-filechecker
+
+
+FROM alpine:3.9
+MAINTAINER 	Dorian Cantzen <cantzen@extrument.com>
+ADD index.html /mountready
+COPY --from=build /src/go-filechecker /usr/local/bin/go-filechecker
+RUN chmod +x /usr/local/bin/go-filechecker
+RUN apk add curl
+
---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+func delaySecond(n time.Duration) {
+	time.Sleep(n * time.Second)
+	checkDir()
+}
+func copyFile(){
+	srcFile, err := os.Open("/mountready/index.html")
+	check(err)
+	defer srcFile.Close()
+
+	destFile, err := os.Create("/www/html/kubertestvolume/index.html") // creates if file doesn't exist
+	check(err)
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, srcFile) // check first var for number of bytes copied
+	check(err)
+
+	err = destFile.Sync()
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println("Error : %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+
+func checkDir(){
+	if _, err := os.Stat("/www/html/kubertestvolume/"); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("Waiting...")
+			delaySecond(10)
+
+		} else {
+			fmt.Println("Other")
+		}
+	}
+}
+
+func main() {
+	checkDir()
+}
